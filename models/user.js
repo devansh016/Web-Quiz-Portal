db453a929b35d302797cd134c2c74247cfaa32ec
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-20 11:54:00 +0530

    Removed extra information from response

--- models/user.js
+++ models/user.js
@@ -9,4 +9,17 @@ const userSchema = new Schema({
     createdDate: { type: Date, default: Date.now }
 });
 
+//To remove extra information
+userSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.password;
+        delete ret.createdDate;
+        delete ret.name;
+        delete ret.email;
+    }
+});
+
 module.exports = mongoose.model('User', userSchema);
