3fac28190a7f5351a42ca823d1c6d1dd24f75ea5
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-20 22:39:19 +0530

    fixed port issue on heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const db = require('./util/database');
 const errorHandler = require('./util/error-handler');
 
 const app = express();
-const port = process.env.port || 80;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
