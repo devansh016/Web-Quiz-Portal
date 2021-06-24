8caf18d91cc6ad437ba2cbf33ecaf11bbc879637
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-24 14:58:53 +0530

    Auto registration in Quiz

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Quiz = require('../models/quiz');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -11,6 +12,7 @@ async function authenticate({ username, password }) {
     if (user && bcrypt.compareSync(password, user.password)) {
         //Creating A Token
         const token = jwt.sign({ sub: user.username }, process.env.JWT_SECRET, { expiresIn: '86400s' });
+
         return {
             ...user.toJSON(),
             token
@@ -33,6 +35,10 @@ async function register(userParam) {
 
     // saving the user
     await user.save();
+
+    // register for quiz
+    const quiz = new Quiz({user_id: user.id, user_name: user.username });
+    await quiz.save ();
 }
 
 module.exports ={
